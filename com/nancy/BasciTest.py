09005fbf342266ed58b46036fb94bc220cf218f0
guangfeng.zhou <guangfeng.zhou@shenmajr.com>
2018-02-11 10:27:59 +0800

    python基础

---
+++ com/nancy/BasciTest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+
+print(11)
+
+
+if True:
+    print ("Answer")
+    print ("True")
+else:
+    print ("Answer")
+    print ("False")    # 缩进不一致，会导致运行错误
